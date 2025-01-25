ae633619e96d8b31b6b17655b16f5202e6cde588
shane-smyth <158466904+shane-smyth@users.noreply.github.com>
2025-01-25 14:06:15 +0000

    created navbar and hero

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -1,20 +1,60 @@
 import React, { Component } from "react"
+import { Link } from "react-router-dom"
 import DisplayAllProducts from "./DisplayAllProducts"
+import axios from "axios"
+import {SERVER_HOST} from "../config/global_constants"
+
 
 export default class Home extends Component {
     constructor(props) {
         super(props)
+
+        this.state = {
+            products: [],
+        }
+    }
+
+    componentDidMount() {
+        axios.get(`${SERVER_HOST}/products`)
+            .then(res => {
+                if (res.data) {
+                    this.setState({products: res.data})
+                }
+                else {
+                    console.log("Records not found.")
+                }
+            })
     }
 
     render() {
         return (
             <div>
                 <div className="hero">
-                    <img src="https://as1.ftcdn.net/v2/jpg/03/08/93/46/1000_F_308934657_5Q7cqp8BQSzrJ9DEJQ7G6bJfGAUJqETl.jpg"/>
+                    <div className="heroDescription grids">
+                        <div className="heroDescriptionBox grids">
+                            <h1>The Music Shop</h1>
+                            <h3>Rock Your World. Loud and Clear.</h3>
+                            <p>Discover the ultimate gear for your music journey-electric guitars,
+                                drum kits, amps, pedals, and everything in between. From stage-ready
+                                essentials to pro-level accessories, we’ve got everything you need
+                                to keep the music alive.</p>
+                            <h3>Unleash Your Sound. Shop Now.</h3>
+                        </div>
+                    </div>
+                    <div className="heroImage grids">
+                        <img src="./images/hero-image.png" alt="hero image"/>
+                    </div>
+                    <div className="heroButton grids">
+                        <div className="heroShopButton">
+                            <Link to="/shop">
+                                <p>Shop</p>
+                            </Link>
+                        </div>
+                    </div>
                 </div>
 
                 <body>
-                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. At corporis ducimus eum exercitationem,
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. At corporis ducimus eum exercitationem,
                         ipsa iste iure, nesciunt nulla, quidem sapiente temporibus velit! Aperiam maxime neque officia,
                         officiis quas sed voluptatum?</p>
 
---
+++ client/src/components/Main.js
@@ -0,0 +1,30 @@
+import React, {Component} from "react"
+
+import NavBar from "./NavBar";
+import Home from "./Home";
+import Footer from "./Footer";
+
+
+export default class Main extends Component {
+    constructor(props) {
+        super(props)
+
+    }
+
+
+    render() {
+        return (
+            <div>
+                <header>
+                    <NavBar/>
+                </header>
+
+                <Home/>
+
+                <footer>
+                    <Footer/>
+                </footer>
+            </div>
+        )
+    }
+}
